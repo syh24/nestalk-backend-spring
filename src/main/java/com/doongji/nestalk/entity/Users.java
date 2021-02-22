208c0f8c32840d4372e3e0153f863b1c0a17f04d
hyojin107 <49690185+hyojin107@users.noreply.github.com>
2021-02-22 22:22:12 +0900

    refactor: users entity 변경

--- src/main/java/com/doongji/nestalk/entity/Users.java
+++ src/main/java/com/doongji/nestalk/entity/Users.java
@@ -1,12 +1,9 @@
 package com.doongji.nestalk.entity;
 
-import com.doongji.nestalk.entity.user.Email;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
-import java.sql.Timestamp;
 
 @Getter
 @RequiredArgsConstructor
@@ -15,13 +12,28 @@ public class Users {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String userId;          //아이디
+
+    @Column(nullable = false)
     private String name;            //이름
+
+    @Column(nullable = false)
     private String password;        //비밀번호
+
+    @Column(nullable = false)
     private String email;           //이메일
+
+    @Column(nullable = false)
     private String phone;           //핸드폰번호
+
+    @Column
     private String profileImage;    //프로필사진
+
+    @Column
     private String backgroundImage; //배경사진
+
+    @Column
     private String stateMessage;    //상태메시지
+
     private String role;
 
 }
