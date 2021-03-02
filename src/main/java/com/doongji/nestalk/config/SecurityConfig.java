5ab785e998bd7002269cd39b361f7f56d5f8e4fd
HwanChang <hwanchang.dev@gmail.com>
2021-03-02 12:52:23 +0900

    feat: 이메일 중복 체크 URL 접근 권한 해제

--- src/main/java/com/doongji/nestalk/config/SecurityConfig.java
+++ src/main/java/com/doongji/nestalk/config/SecurityConfig.java
@@ -77,6 +77,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .authorizeRequests()
                 .antMatchers("/api/auth").permitAll()
+                .antMatchers("/api/v?/user/exists").permitAll()
                 .antMatchers("/api/v?/user/join").permitAll()
                 .antMatchers("/api/v?/**").hasRole(Role.USER.name())
                 .anyRequest().authenticated()
