1da490efc46040a38ded76b2acf9178c12ecdc6f
syh <dbsgur6896@naver.com>
2021-03-04 15:33:01 +0900

    feat: 권한 수정

--- src/main/java/com/doongji/nestalk/config/SecurityConfig.java
+++ src/main/java/com/doongji/nestalk/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -78,7 +79,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/api/auth").permitAll()
                 .antMatchers("/api/v?/user/exists").permitAll()
-                .antMatchers("/api/v?/user/password").permitAll()
+                .antMatchers(HttpMethod.POST,"/api/v?/user/password").permitAll()
                 .antMatchers("/api/v?/user/join").permitAll()
                 .antMatchers("/api/v?/**").hasRole(Role.USER.name())
                 .anyRequest().authenticated()
