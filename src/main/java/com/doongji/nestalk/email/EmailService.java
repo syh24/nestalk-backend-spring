754b4b8976407a35a482b3d4f5a9f9d51c08f921
syh <dbsgur6896@naver.com>
2021-03-05 12:11:50 +0900

    refactor: UUID를 이용한 임시 비밀번호 생성

--- src/main/java/com/doongji/nestalk/email/EmailService.java
+++ src/main/java/com/doongji/nestalk/email/EmailService.java
@@ -2,15 +2,12 @@ package com.doongji.nestalk.email;
 
 import com.doongji.nestalk.entity.user.User;
 import com.doongji.nestalk.error.NotFoundException;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
-import java.util.NoSuchElementException;
-import java.util.Random;
+import java.util.UUID;
 
 
 @RequiredArgsConstructor
@@ -28,26 +25,9 @@ public class EmailService {
     }
 
     public String sendTemporaryPassword(User user) throws NotFoundException{
-            StringBuffer temp = new StringBuffer();
-            Random rnd = new Random();
-            for (int i = 0; i < 10; i++) {
-                int rIndex = rnd.nextInt(3);
-                switch (rIndex) {
-                    case 0:
-                        // a-z
-                        temp.append((char) ((int) (rnd.nextInt(26)) + 97));
-                        break;
-                    case 1:
-                        // A-Z
-                        temp.append((char) ((int) (rnd.nextInt(26)) + 65));
-                        break;
-                    case 2:
-                        // 0-9
-                        temp.append((rnd.nextInt(10)));
-                        break;
-                }
-            }
-            mailSend(user.getEmail(),"[NESTALK] 임시 비밀번호 입니다.","임시 비밀번호는 :" + temp.toString() + "입니다.");
-            return temp.toString();
+        UUID uuid = UUID.randomUUID();
+        String temp = uuid.toString().substring(0, 8);
+        mailSend(user.getEmail(),"[NESTALK] 임시 비밀번호 입니다.","임시 비밀번호는 :" + temp + "입니다.");
+        return temp;
     }
 }
