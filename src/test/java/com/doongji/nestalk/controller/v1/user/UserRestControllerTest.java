3993aac7c90534e4000bd6561221bbc8431ee7d8
syh <dbsgur6896@naver.com>
2021-03-13 22:30:39 +0900

    refactor: mockito 사용

--- src/test/java/com/doongji/nestalk/controller/v1/user/UserRestControllerTest.java
+++ src/test/java/com/doongji/nestalk/controller/v1/user/UserRestControllerTest.java
@@ -1,83 +1,62 @@
 package com.doongji.nestalk.controller.v1.user;
 
 import com.doongji.nestalk.controller.v1.user.dto.FindEmailRequest;
-import com.doongji.nestalk.entity.user.User;
-import com.doongji.nestalk.repository.user.UserRepository;
+import com.doongji.nestalk.service.user.UserService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 import javax.transaction.Transactional;
 
-import java.time.LocalDate;
-
-import static org.assertj.core.api.Assertions.*;
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@ExtendWith (SpringExtension.class)
-@SpringBootTest (webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest
 @Transactional
+@Slf4j
 class UserRestControllerTest {
 
-    @LocalServerPort
-    private int port;
+    @InjectMocks
+    private UserRestController userRestController;
 
-    @Autowired
-    private WebApplicationContext context;
+    @Mock
+    private UserService userService;
+
 
     private MockMvc mvc;
 
-    @Autowired
-    private UserRepository userRepository;
-
     @BeforeEach
-    public void before() {
+    public void setUp() {
         mvc = MockMvcBuilders
-                .webAppContextSetup(context)
-                .apply(springSecurity())
+                .standaloneSetup(userRestController)
                 .build();
     }
 
     @Test
-    void 아이디_찾기_성공테스트() throws Exception {
-        User user = joinUser();
+    @DisplayName("아이디 찾기 테스트")
+    void find() throws Exception {
+        String expectEmail = "test@gmail.com";
         FindEmailRequest request = new FindEmailRequest("test", "010-1234-5678");
-        String url = "http://localhost:" + port + "/api/v1/user/email";
-        MvcResult result = mvc.perform(post(url)
+        given(userService.findUserByNameAndPhone(ArgumentMatchers.any(),ArgumentMatchers.any())).willReturn(expectEmail);
+        final ResultActions actions = mvc.perform(post("/api/v1/user/email")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(new ObjectMapper().writeValueAsString(request)))
+                .content(new ObjectMapper().writeValueAsString(request)));
+
+        actions
                 .andExpect(status().isOk())
-                .andReturn();
-        assertThat(result.getResponse().getContentAsString()).contains(user.getEmail());
-    }
-
-    @Test
-    void 아이디_찾기_실패테스트() throws Exception {
-        User user = joinUser();
-        FindEmailRequest request = new FindEmailRequest("ttt", "010-1234-5678");
-        String url = "http://localhost:" + port + "/api/v1/user/email";
-        MvcResult result = mvc.perform(post(url)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(new ObjectMapper().writeValueAsString(request)))
-                .andExpect(status().is4xxClientError())
-                .andReturn();
-        assertThat(result.getResponse().getContentAsString()).doesNotContain(user.getEmail());
-    }
-
-    public User joinUser(){
-        return userRepository.save(new User("abc@gmail.com","test","123456789","010-1234-5678", LocalDate.now()));
+                .andExpect(jsonPath("$.email").value(expectEmail));
     }
 
 }
